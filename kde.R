ffb72e5f8761196f9aaae5d278be8a49179943d4
John Myles White <jmw@johnmyleswhite.com>
2010-11-08 09:40:57 -0500

    Added labels to plots; split by time unit

--- kde.R
+++ kde.R
@@ -1,8 +1,20 @@
 library('ProjectTemplate')
 load.project()
 
-pdf(file.path('graphs', 'gender.pdf'))
+png(file.path('graphs', 'minutes_gender.png'))
 ggplot(times, aes(x = Time, color = Gender, fill = Gender, group = Gender)) +
   geom_density() +
-  facet_grid(Gender ~ .)
+  facet_grid(Gender ~ .) +
+  xlab('Total Time in Minutes') +
+  ylab('Estimated Probability Density') +
+  opts(title = 'Comparing Men and Women in the NYC Marathon')
+dev.off()
+
+png(file.path('graphs', 'hours_gender.png'))
+ggplot(times, aes(x = Time / 60, color = Gender, fill = Gender, group = Gender)) +
+  geom_density() +
+  facet_grid(Gender ~ .) +
+  xlab('Total Time in Hours') +
+  ylab('Estimated Probability Density') +
+  opts(title = 'Comparing Men and Women in the NYC Marathon')
 dev.off()
